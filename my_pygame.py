7d13b250910faf74c7e24e0a402983ef028fbd44
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-09-25 18:24:18 +0700

    chuyển động bird

--- my_pygame.py
+++ my_pygame.py
@@ -40,6 +40,19 @@ def check_collision(pipes):
     if bird_rect.top <= -75 or bird_rect.bottom >= 650:
         return False
     return True
+# End xử lí va chạm
+
+# Xử lí đập cánh chim
+def bird_animation():
+    new_bird = bird_list[bird_index]
+    new_bird_rect = new_bird.get_rect(center=(100, bird_rect.centery))
+    return new_bird, new_bird_rect
+
+
+#  Xoay chim
+def rotate_bird(bird1):
+    new_bird = pygame.transform.rotozoom(bird1, -bird_movement * 3, 1)
+    return new_bird
 
 
 pygame.init()
@@ -61,21 +74,38 @@ current_dir = os.path.dirname(__file__)
 
 # Tải hình nền
 bg_path = os.path.join(current_dir, "assets", "background-night.png")
-bg = pygame.image.load(bg_path)
+bg = pygame.image.load(bg_path).convert()
 bg = pygame.transform.scale2x(bg)
 
 # Cho sàn nhà
 floor_path = os.path.join(current_dir, "assets", "floor.png")
-floor = pygame.image.load(floor_path)
+floor = pygame.image.load(floor_path).convert()
 floor = pygame.transform.scale2x(floor)
 floor_x_pos = 0
 
 # Tạo chim
-bird_path = os.path.join(current_dir, "assets", "yellowbird-midflap.png")
-bird = pygame.image.load(bird_path)
-bird = pygame.transform.scale2x(bird)
+
+bird_path_down = os.path.join(current_dir, "assets", "yellowbird-downflap.png")
+bird_down = pygame.image.load(bird_path_down).convert_alpha()
+bird_down = pygame.transform.scale2x(bird_down)
+
+bird_path_mid = os.path.join(current_dir, "assets", "yellowbird-midflap.png")
+bird_mid = pygame.image.load(bird_path_mid).convert_alpha()
+bird_mid = pygame.transform.scale2x(bird_mid)
+
+bird_path_up = os.path.join(current_dir, "assets", "yellowbird-upflap.png")
+bird_up = pygame.image.load(bird_path_up).convert_alpha()
+bird_up = pygame.transform.scale2x(bird_up)
+
+#  Trạng thái đập cánh
+bird_list = [bird_down, bird_mid, bird_up]  # 0,1,2
+bird_index = 0
+bird = bird_list[bird_index]
 bird_rect = bird.get_rect(center=(100, 384))
 
+# Tạo timer cho bird
+birdflap = pygame.USEREVENT + 1
+pygame.time.set_timer(birdflap, 200)
 # Tạo ống
 pipe_surface_path = os.path.join(current_dir, "assets", "pipe-green.png")
 pipe_surface = pygame.image.load(pipe_surface_path)
@@ -87,7 +117,7 @@ spawnpipe = pygame.USEREVENT
 pygame.time.set_timer(spawnpipe, 1200)
 pipe_height = [200, 300, 400]
 
-
+# While loop của trò chơi
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -105,13 +135,20 @@ while True:
         if event.type == spawnpipe:
             # append những ống mới vào list
             pipe_list.extend(create_pipe())
+        if event.type == birdflap:
+            if bird_index < 2:
+                bird_index += 1
+            else:
+                bird_index = 0
+            bird, bird_rect = bird_animation()
     # Background
     screen.blit(bg, (0, 0))
     if game_active:
         # Của chim
         bird_movement += gravity
+        rotated_bird = rotate_bird(bird)
         bird_rect.centery += bird_movement
-        screen.blit(bird, bird_rect)
+        screen.blit(rotated_bird, bird_rect)
         game_active = check_collision(pipe_list)
         # Ống
         pipe_list = move_pipe(pipe_list)
